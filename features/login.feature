737b598ad00271d31e2c28fad7c79b7e288a163f
LeonNjoroge <leonmuraguri@gmail.com>
2025-02-19 15:20:24 +0300

    Use environment variables

--- features/login.feature
+++ features/login.feature
@@ -1,11 +1,11 @@
 Feature: Login functionality
   Scenario: User logs in with valid credentials
     Given I am on the login page
-    When I enter the "neolzoovie@gmail.com" and "password1"
+    When I enter the email: "" and password: ""
     When I click the login page
     When I complete human verification
     Then I should be redirected to the dashboard
-    Scenario: User logs in with invalid details
+  Scenario: User logs in with invalid details
       Given I am on the login page
-      When I enter the "neooloovie@gmail.com" and "password20"
+      When I enter the wrong email: "" and wrong password: ""
       Then I should get an error message
--- step_definitions/login.steps.ts
+++ step_definitions/login.steps.ts
@@ -1,16 +1,40 @@
+import dotenv from 'dotenv';
+dotenv.config();
+const { I } = inject();
+
+
+/*
+const args = process.argv.slice(3);
+
+const findValue = (valueName) =>{
+    const arg = args.find(arg => arg.startsWith(`--${valueName}=`));
+    return arg ? arg.split('=')[1] : null;
+}
+
+
+const email = findValue('email') || process.env.EMAIL;
+const password = findValue('password') || process.env.PASSWORD;
+*/
+const email = process.env.EMAIL;
+const password = process.env.PASSWORD;
+const wrongEmail = process.env.WRONGMAIL;
+const wrongPass = process.env.WRONGPASS;
+
+
+
 //LOGIN
 //Successfull test
 Given("I am on the login page", ()=>{
     I.amOnPage('/login');
     I.wait(3);
 });
-When("I enter the {string} and {string}", (email, password)=>{
-  I.fillField("Email", email);
-  I.fillField("Password", password);
+When("I enter the email: {string} and password: {string}", (emailIn, passwordIn)=>{
+  I.fillField("Email", emailIn || email);
+  I.fillField("Password", passwordIn || password);
 });
 When("I click the login page", ()=>{
   I.click(".login-button");
-  I.wait(30);
+  I.wait(10);
 });
 
 When("I complete human verification", async () => {
@@ -29,9 +53,9 @@ Given("I am on the login page", ()=>{
   I.amOnPage('/login');
   I.wait(3);
 });
-When("I enter the {string} and {string}", (email, password)=>{
-I.fillField("Email", email);
-I.fillField("Password", password);
+When("I enter the wrong email: {string} and wrong password: {string}", (emailIn, passwordIn)=>{
+I.fillField("Email", emailIn || wrongEmail);
+I.fillField("Password", passwordIn || wrongPass);
 });
 
 Then("I should get an error message", ()=>{
